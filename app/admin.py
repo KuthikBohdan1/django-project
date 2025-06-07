5cb5c34bad4525ad199c8ba2ebfc8b7df0872c9e
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-07 12:32:48 +0300

    2

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from app.models import Reservation, Room, TypeRoom
+# from app.models
+class reservationAdmin(admin.ModelAdmin):
+    list_display =('room, 'date_start', 'phone')
+    search_fileds = ('room', 'date')
 
 admin.site.register(Room)
 admin.site.register(Reservation)
 admin.site.register(TypeRoom)
-# Register your models here.
+
