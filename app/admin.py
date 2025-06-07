1cca1845cf60ad85271b9876ba56dae444443dec
KuthikBohdan1 <bkuzik14@gmail.com>
2025-06-07 12:19:46 +0300

    1

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from app.models import Reservation, Room, TypeRoom
+
+admin.site.register(Room)
+admin.site.register(Reservation)
+admin.site.register(TypeRoom)
 # Register your models here.
